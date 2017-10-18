84e2f3f68dbec9776a5c5035e94ac4fdadecf86c
dciano <dciano@new-host.station>
2017-10-19 01:51:51 +0200

    GEO FIX

--- mandis/settings.py
+++ mandis/settings.py
@@ -12,8 +12,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
